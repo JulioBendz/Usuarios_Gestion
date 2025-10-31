d69db5e5605b1889e29ccef3dbce142e6087d5ea
JulioBendz <bengutjul2@gmail.com>
2025-10-30 20:09:47 -0500

    Add AppDbContext class for Entity Framework Core setup with Usuarios and Area entities

---
+++ Usuarios_Gestion/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Usuarios_Gestion.Models;
+
+namespace Usuarios_Gestion.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuarios> Usuario { get; set; }
+        public DbSet<Area> Area { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.ToTable("Usuario");
+                entity.HasKey(e => e.Codigo);
+                entity.Property(e => e.Nombre).HasMaxLength(50).IsRequired();
+                entity.Property(e => e.Contrasena).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.Fecha).HasColumnType("date");
+
+                // <-- usar la propiedad CodigoArea y mapear a "Codigo_Area"
+                entity.Property(e => e.CodigoArea).HasColumnName("Codigo_Area");
+                entity.HasOne(e => e.Area)
+                      .WithMany()
+                      .HasForeignKey(e => e.CodigoArea)
+                      .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<Area>(entity =>
+            {
+                entity.ToTable("Area");
+                entity.HasKey(a => a.Codigo);
+                entity.Property(a => a.Nombre).HasMaxLength(200).IsRequired();
+            });
+        }
+    }
+}

5eac7367b74bd13666364535c318bade07ef85fc
JulioBendz <bengutjul2@gmail.com>
2025-10-30 20:10:00 -0500

    Add Usuarios model with properties Codigo, Nombre, Contrasena, Fecha, and CodigoArea

---
+++ Usuarios_Gestion/Models/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Usuarios_Gestion.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
+        public string Contrasena { get; set; } = string.Empty;
+
+        [Column(TypeName = "date")]
+        public DateTime Fecha { get; set; }
+
+        // Propiedad C# renombrada a CodigoArea, mapeada a la columna SQL "Codigo_Area"
+        [Column("Codigo_Area")]
+        public int CodigoArea { get; set; }
+
+        [ForeignKey(nameof(CodigoArea))]
+        public Area? Area { get; set; }
+    }
+}
